007ebe0e2bb06f9fd0195b2465350b36e178e0f3
evanelang <33497277+evanelang@users.noreply.github.com>
2017-12-02 18:02:59 -0500

    Add files via upload

---
+++ welcomeScreen.java
@@ -0,0 +1,24 @@
+package com.example.evanlang.ec326;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class welcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+    public void goToMain(View view){
+
+    }
+
+
+
+
+    public void editMode(View view){
+
+    }
+}
